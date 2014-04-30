eb5bd937a2bdd652fcea8b66ac0fd91d43e0ba90
Benjamin Franzke <bfr@qbus.de>
2014-04-30 11:13:04 +0200

    Allow creating new gridelements inline

--- ext_tables.php
+++ ext_tables.php
@@ -1,4 +1,8 @@
 <?php if (!defined('TYPO3_MODE')) die ('Access denied.');
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($_EXTKEY, 'Configuration/TypoScript', 'Qbus Tools');
+
+$TCA['tt_content']['columns']['tx_gridelements_children']['config']['appearance']['enabledControls']['new'] = true;
+$TCA['tt_content']['columns']['tx_gridelements_children']['config']['appearance']['levelLinksPosition'] = 'top';
+
 ?>
