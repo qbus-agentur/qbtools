e24b876ebccb58424e617a42e746f97f75d6945f
Benjamin Franzke <bfr@qbus.de>
2014-05-08 18:52:38 +0200

    Mailform: Make use of fieldNamePrefix

--- Classes/ViewHelpers/Widget/MailformViewHelper.php
+++ Classes/ViewHelpers/Widget/MailformViewHelper.php
@@ -48,8 +48,15 @@ class MailformViewHelper extends \TYPO3\CMS\Fluid\Core\Widget\AbstractWidgetView
 	 * @return string
 	 */
 	public function render($recipient, $sender = null, $required = array("firstname", "lastname", "email"),
-		$mailTemplate = "EXT:qbtools/Resources/Private/Templates/Mailform/Mail.txt") {
-		return $this->initiateSubRequest();
+			$mailTemplate = "EXT:qbtools/Resources/Private/Templates/Mailform/Mail.txt") {
+
+		/* <f:renderChildren> does not include the variable context from the  subrequest-controller,
+		 * therefore we set the desired variables here. */
+		$this->viewHelperVariableContainer->add('TYPO3\\CMS\\Fluid\\ViewHelpers\\FormViewHelper', 'fieldNamePrefix', "msg");
+		$result = $this->initiateSubRequest();
+		$this->viewHelperVariableContainer->remove('TYPO3\\CMS\\Fluid\\ViewHelpers\\FormViewHelper', 'fieldNamePrefix');
+
+		return $result;
 	}
 }
 
