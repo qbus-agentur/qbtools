e7e446997a0f2bb0659cda61d81f78f36455e57a
Benjamin Franzke <benjaminfranzke@googlemail.com>
2014-09-29 08:41:52 +0200

    Add CalculateBoundsViewHelper
    Can be used by galleries to obtain the bounds of all the images

---
+++ Classes/ViewHelpers/CalculateBoundsViewHelper.php
@@ -0,0 +1,53 @@
+<?php
+namespace Qbus\Qbtools\ViewHelpers;
+
+class CalculateBoundsViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper {
+
+	/**
+	 * @param array $images
+	 *
+	 * @return array
+	 */
+	public function render($images) {
+		$result = array("minWidth" => 0, "minHeight" => 0,
+				"maxWidth" => 0, "maxHeight" => 0,
+				"minRatio" => 0, "maxRatio"  => 0,
+				"minRatioWidth" => 0, "minRatioHeight"  => 0,
+				"maxRatioWidth" => 0, "maxRatioHeight"  => 0);
+
+		foreach ($images as $image) {
+			foreach (array("height", "width") as $type) {
+				$val = $image->getProperty($type);
+				if ($val === NULL)
+					continue;
+
+				$min = 'min'.ucfirst($type);
+				$max = 'max'.ucfirst($type);
+
+				if ($result[$min] == 0 || $result[$min] > $val)
+					$result[$min] = $val;
+
+				if ($result[$max] < $val)
+					$result[$max] = $val;
+			}
+			$width = $image->getProperty("width");
+			$height = $image->getProperty("height");
+			if ($width === NULL || $height === NULL || $width == 0)
+				continue;
+
+			$ratio = $height / $width * 100;
+			if ($result["minRatio"] == 0 || $ratio < $result["minRatio"]) {
+				$result["minRatio"] = $ratio;
+				$result["minRatioWidth"] = $width;
+				$result["minRatioHeight"] = $height;
+			}
+			if ($ratio > $result["maxRatio"]) {
+				$result["maxRatio"] = $ratio;
+				$result["maxRatioWidth"] = $width;
+				$result["maxRatioHeight"] = $height;
+			}
+		}
+
+		return $result;
+	}
+}
