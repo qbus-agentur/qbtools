11c3b6dff0a99c0630a30ae83cbbc18dabfc9f1d
Benjamin Franzke <bfr@qbus.de>
2016-05-25 08:39:32 +0200

    Mailform: Add some examples in the classes phpdoc

--- Classes/ViewHelpers/Widget/MailformViewHelper.php
+++ Classes/ViewHelpers/Widget/MailformViewHelper.php
@@ -2,6 +2,27 @@
 namespace Qbus\Qbtools\ViewHelpers\Widget;
 
 /**
+ * Mailform widget with configurable form fields
+ *
+ * Example:
+ *
+ * {namespace q=Qbus\Qbtools\ViewHelpers}
+ * <q:widget.mailform recipient="{mail: 'receiver@example.com', name: 'Receiver Name'}"
+ *   required="{0: 'firstname', 1: 'lastname', 2: 'email'}"
+ *   mailTemplate="EXT:your_site/Resources/Private/Templates/Mailform/Mail.txt">
+ *   <div class="qbtools-mailform-notification">
+ *     <div class="alert alert-success" data-status="success" style="display: none">You message has not been sent.</div>
+ *     <div class="alert alert-danger" data-status="fields-missing" style="display: none">Fields marked with * have to be filled.</div>
+ *     <div class="alert alert-danger" data-status="error" style="display: none">Your E-Mail could not be sent. An internal error happended.</div>
+ *   </div>
+ *   <f:form.textfield name="firstname" required="true" id="{qbmailformid}-firstname" class="form-control" placeholder="Firstname *" />
+ *   <f:form.textfield name="lastname"  required="true" id="{qbmailformid}-lastname"  class="form-control" placeholder="Lastname *" />
+ *   <f:form.textfield name="email"     required="true" type="email" id="{qbmailformid}-email" class="form-control" placeholder="E-Mail *"/>
+ *   <f:form.submit value="Anfrage absenden"/>
+ *  </q:widget.mailform>
+ *
+ *  NOTE: You may set config.tx_qbtools.mailform.receiver.overwrite.email = your@dev.email
+ *
  * TODO: accept required as string-list
  * TODO: accept required list with conditions?
  * TODO: accept recipient as simple string containing only the mail address
