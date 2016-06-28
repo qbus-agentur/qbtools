cd2498c553cd530a4f7325cb5379d971a1c82aac
Benjamin Franzke <bfr@qbus.de>
2016-06-28 23:45:38 +0200

    Add wrap view helper

---
+++ Classes/ViewHelpers/WrapViewHelper.php
@@ -0,0 +1,26 @@
+<?php
+namespace Qbus\Qbtools\ViewHelpers;
+
+/**
+ * WrapViewHelper
+ *
+ * @author Benjamin Franzke <bfr@qbus.de>
+ * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
+ */
+class WrapViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
+{
+
+    /**
+     * @param string $class
+     * @return string
+     */
+    public function render($class)
+    {
+        $content = $this->renderChildren();
+        if (ctype_space($content) || $content === '') {
+            return '';
+        }
+
+        return '<div class="' . htmlspecialchars($class) . '">' . $content . '</div>';
+    }
+}
