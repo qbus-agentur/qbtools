ada7740fa5a13f2de13d636d7a4ea2fd61d0fa2d
Benjamin Franzke <benjaminfranzke@gmail.com>
2019-03-06 15:36:56 +0100

    Refactor CacheTagViewHelper for TYPO3 v9

--- Classes/ViewHelpers/CacheTagViewHelper.php
+++ Classes/ViewHelpers/CacheTagViewHelper.php
@@ -1,21 +1,39 @@
 <?php
 namespace Qbus\Qbtools\ViewHelpers;
 
+use TYPO3Fluid\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3Fluid\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3Fluid\Fluid\Core\ViewHelper\Traits\CompileWithRenderStatic;
+
 /**
  * CacheTagViewHelper
  *
  * @author Benjamin Franzke <bfr@qbus.de>
  * @license http://www.gnu.org/licenses/gpl.html GNU General Public License, version 3 or later
  */
-class CacheTagViewHelper extends \TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper
+class CacheTagViewHelper extends AbstractViewHelper
 {
+    use CompileWithRenderStatic;
+
     /**
-     * @param string $tag
+     * Initialize arguments
      */
-    public function render($tag)
+    public function initializeArguments()
     {
-        $GLOBALS['TSFE']->addCacheTags([$tag]);
-        return '';
+        $this->registerArgument('tag', 'string', '', true);
     }
 
+    /**
+     * @param array $arguments
+     * @param \Closure $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     * @return string
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    {
+        $tag = $arguments['tag'];
+        $GLOBALS['TSFE']->addCacheTags([$tag]);
+
+        return '';
+    }
 }
