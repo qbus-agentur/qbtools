06283d5d8e5879a3a10391c647265df36ff73518
Benjamin Franzke <benjaminfranzke@gmail.com>
2019-03-07 14:20:28 +0100

    ContentObjectGetDataHook: Use use statements where possible

--- Classes/Hooks/ContentObjectGetDataHook.php
+++ Classes/Hooks/ContentObjectGetDataHook.php
@@ -2,29 +2,37 @@
 namespace Qbus\Qbtools\Hooks;
 
 use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Service\FlexFormService;
 use TYPO3\CMS\Extbase\Utility\ArrayUtility;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectGetDataHookInterface;
+use TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer;
 
-class ContentObjectGetDataHook implements \TYPO3\CMS\Frontend\ContentObject\ContentObjectGetDataHookInterface
+class ContentObjectGetDataHook implements ContentObjectGetDataHookInterface
 {
     /**
      * Extends the getData()-Method of \TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer to process more/other commands
      *
-     * @param  string                                                  $getDataString Full content of getData-request e.g. "TSFE:id // field:title // field:uid
-     * @param  array                                                   $fields        Current field-array
-     * @param  string                                                  $sectionValue  Currently examined section value of the getData request e.g. "field:title
-     * @param  string                                                  $returnValue   Current returnValue that was processed so far by getData
-     * @param  \TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer $parentObject  Parent content object
-     * @return string                                                  Get data result
+     * @param  string                $getDataString Full content of getData-request e.g. "TSFE:id // field:title // field:uid
+     * @param  array                 $fields        Current field-array
+     * @param  string                $sectionValue  Currently examined section value of the getData request e.g. "field:title
+     * @param  string                $returnValue   Current returnValue that was processed so far by getData
+     * @param  ContentObjectRenderer $parentObject  Parent content object
+     * @return string                Get data result
      */
-    public function getDataExtension($getDataString, array $fields, $sectionValue, $returnValue, \TYPO3\CMS\Frontend\ContentObject\ContentObjectRenderer &$parentObject)
-    {
+    public function getDataExtension(
+        $getDataString,
+        array $fields,
+        $sectionValue,
+        $returnValue,
+        ContentObjectRenderer &$parentObject
+    ) {
         $parts = explode(':', $sectionValue, 2);
         $type = strtolower(trim($parts[0]));
         $key = trim($parts[1]);
 
         switch ($type) {
             case 'qbtools_flexform_field':
-                $flexform_service = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Service\\FlexFormService');
+                $flexform_service = GeneralUtility::makeInstance(FlexFormService::class);
                 $flexform_fields = $flexform_service->convertFlexFormContentToArray($fields['pi_flexform']);
                 $returnValue = ArrayUtility::getValueByPath($flexform_fields, $key);
                 break;
