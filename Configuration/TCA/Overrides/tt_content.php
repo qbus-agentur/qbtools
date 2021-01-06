83720e31b5050e6ac5ed0d82d22a937d96ccaf4c
Janine Schendel <jsc@qbus.de>
2021-01-06 12:24:39 +0100

    Change location of TCA and Typoscript templates to Configuration

---
+++ Configuration/TCA/Overrides/tt_content.php
@@ -0,0 +1,5 @@
+<?php
+
+$GLOBALS['TCA']['tt_content']['columns']['tx_gridelements_children']['config']['appearance']['enabledControls']['new'] = true;
+$GLOBALS['TCA']['tt_content']['columns']['tx_gridelements_children']['config']['appearance']['levelLinksPosition'] = 'top';
+
--- ext_tables.php
+++ ext_tables.php
@@ -3,9 +3,4 @@ if (!defined('TYPO3_MODE')) {
     die('Access denied.');
 }
 
-\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile('qbtools', 'Configuration/TypoScript', 'Qbus Tools');
-
-$TCA['tt_content']['columns']['tx_gridelements_children']['config']['appearance']['enabledControls']['new'] = true;
-$TCA['tt_content']['columns']['tx_gridelements_children']['config']['appearance']['levelLinksPosition'] = 'top';
-
 $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['BackendLayoutDataProvider']['qbtools'] = 'Qbus\\Qbtools\\Hooks\\Options\\BackendLayoutDataProvider';
