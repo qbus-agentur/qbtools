d01de6c62b2192e561c94bab83c64c1b7f0cdb71
Benjamin Franzke <bfr@qbus.de>
2021-10-04 07:47:23 +0200

    Fix usage without EXT:gridelements

--- Configuration/TCA/Overrides/tt_content.php
+++ Configuration/TCA/Overrides/tt_content.php
@@ -1,5 +1,7 @@
 <?php
 
-$GLOBALS['TCA']['tt_content']['columns']['tx_gridelements_children']['config']['appearance']['enabledControls']['new'] = true;
-$GLOBALS['TCA']['tt_content']['columns']['tx_gridelements_children']['config']['appearance']['levelLinksPosition'] = 'top';
+if (isset($GLOBALS['TCA']['tt_content']['columns']['tx_gridelements_children'])) {
+	$GLOBALS['TCA']['tt_content']['columns']['tx_gridelements_children']['config']['appearance']['enabledControls']['new'] = true;
+	$GLOBALS['TCA']['tt_content']['columns']['tx_gridelements_children']['config']['appearance']['levelLinksPosition'] = 'top';
+}
 
